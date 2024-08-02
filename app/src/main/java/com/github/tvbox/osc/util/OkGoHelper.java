35914113fac23b5baa2dc4b6bfe604b380bd1cf9
shilemeia <993686578@qq.com>
2024-08-02 23:52:45 +0800

    更新 OkGoHelper.java

--- app/src/main/java/com/github/tvbox/osc/util/OkGoHelper.java
+++ app/src/main/java/com/github/tvbox/osc/util/OkGoHelper.java
@@ -85,16 +85,6 @@ public class OkGoHelper {
             case 3: {
                 return "https://doh.360.cn/dns-query";
             }
-            case 4: {
-                // return "https://1.1.1.1/dns-query";   // takagen99 - removed Cloudflare
-                return "https://dns.google/dns-query";
-            }
-            case 5: {
-                return "https://dns.adguard.com/dns-query";
-            }
-            case 6: {
-                return "https://dns.quad9.net/dns-query";
-            }
         }
         return "";
     }
@@ -104,9 +94,6 @@ public class OkGoHelper {
         dnsHttpsList.add("腾讯");
         dnsHttpsList.add("阿里");
         dnsHttpsList.add("360");
-        dnsHttpsList.add("Google");
-        dnsHttpsList.add("AdGuard");
-        dnsHttpsList.add("Quad9");
         OkHttpClient.Builder builder = new OkHttpClient.Builder();
         HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor("OkExoPlayer");
         if (Hawk.get(HawkConfig.DEBUG_OPEN, false)) {
